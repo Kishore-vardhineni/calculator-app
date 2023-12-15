dc0b67577e25d0d2f4c3e79a431fa3e6e5a36be2
Kishore-vardhineni <kishore.vardhineni01@gmail.com>
2023-12-15 17:06:59 +0530

    Calculator app with css changes

--- src/App.js
+++ src/App.js
@@ -1,39 +1,33 @@
 import React, {useState} from 'react';
 import './App.css';
+import Keypad from './keypad';
 
 function App() {
-  const [input, setInput] = useState('');
-  const [result, setResult] = useState(0);
+  
+  let [input, setInput] = useState("")
 
-  const changeHandler = e => {
-    setInput(e.target.value);
+  function handleClick(value) {
+     setInput(input + value)
+  }
+
+  function calculate(value) {
+    setInput(eval(input + value))
+  }
+
+  function handleClear() {
+    setInput("");
   }
 
   return (
-    <div className='App'>
-      <input type="text" name="input" value={input} onChange={changeHandler}/> <br/>
-      <button onClick={() => setResult(eval(input))}>Result</button> <br/>
-
-      <h4><b>Result is:</b> {result}</h4>
-
-      <button onClick={() => setInput(input+'1')}>1</button>
-      <button onClick={() => setInput(input+'2')}>2</button>
-      <button onClick={() => setInput(input+'3')}>3</button>
-      <button onClick={() => setInput(input+'4')}>4</button>
-      <button onClick={() => setInput(input+'5')}>5</button><br/>
-
-      <button onClick={() => setInput(input+'6')}>6</button>
-      <button onClick={() => setInput(input+'7')}>7</button>
-      <button onClick={() => setInput(input+'8')}>8</button>
-      <button onClick={() => setInput(input+'9')}>9</button>
-      <button onClick={() => setInput(input+'0')}>0</button><br/>
-
-      <button onClick={() => setInput(input+'+')}>+</button>
-      <button onClick={() => setInput(input+'-')}>-</button>
-      <button onClick={() => setInput(input+'*')}>*</button>
-      <button onClick={() => setInput(input+'/')}>/</button>
-      <button onClick={() => setInput('')}>Clr</button>
-    </div>
+ 
+     <div className='container'>
+        <h1>Calculator App using react</h1>
+        <div className='calculator'>
+            <input type='text'className='output' value={input}/>
+            <Keypad handleClick={handleClick} calculate={calculate} handleClear={handleClear}/>
+        </div>
+     </div>
+    
   );
 }
 
